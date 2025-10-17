26072d904899d2e245eb29fb91204af359b1af02
Maximilian Ulmer <45373877+mwulmer@users.noreply.github.com>
2025-10-17 22:59:38 +0200

    Fix URL in setup.py for project repository

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="ocdit",
     version="0.0.1",
-    url="https://github.com/DLR-RM/oc-dit/settings",
+    url="https://github.com/DLR-RM/oc-dit",
     author="Maximilian Ulmer",
     author_email="maximilian.ulmer@dlr.de",
     description="",
